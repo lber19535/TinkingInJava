bb57e476020eb91fa16dc6ba2557f189784aa796
lber119535 <lber19535@126.com>
2014-12-24 15:05:28 +0800

    Concurrency Control learning day 2: 8. thread sleep

---
+++ ConcurrencyControl/src/com/bill/example/SleepingTask.java
@@ -0,0 +1,32 @@
+package com.bill.example;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * the print is not sort by some seq, the seq depend on the platform and jvm type
+ * Created by Bill on 2014/12/22.
+ */
+public class SleepingTask extends LiftOff{
+
+    @Override
+    public void run() {
+        while (countDown-- > 0){
+            System.out.print(status());
+            try {
+                TimeUnit.MICROSECONDS.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            exec.execute(new SleepingTask());
+        }
+        exec.shutdown();
+    }
+}
