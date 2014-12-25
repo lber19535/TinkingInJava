b64a68647cae4ab43c38853253ac5c86a5c22830
lber119535 <lber19535@126.com>
2014-12-25 15:01:11 +0800

    Concurrency Control learning day 2: 10. thread priority and yield method study

--- ConcurrencyControl/src/com/bill/example/SimplePriorities.java
+++ ConcurrencyControl/src/com/bill/example/SimplePriorities.java
@@ -4,8 +4,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * this demo shows that the set priority method is not work fine,
- * so do not try to use priority to change thread order
+ * This demo shows that the set priority method is not work fine,
+ * so do not try to use priority to change thread order.
+ *
+ * When i read <thinking in java>, author write that priority and yield
+ * can run with correct moment, but java 6 and java 5 have difference release
+ * in Hotspot, that makes these two methods will not work fine with what your think.
+ * So, I google some information about these, and the result is practically never use
+ * these two method to try to influence threads action.
  *
  * Created by Bill on 2014/12/24.
  */
@@ -21,10 +27,12 @@ public class SimplePriorities implements Runnable {
 
     @Override
     public void run() {
+        // the priority is not work fine and look like not useful
         Thread.currentThread().setPriority(priority);
         while (true){
             for (int i = 0; i < 1000000; i++) {
                 d += (Math.PI + Math.E) / (double)i;
+                // if comment the yield method, the threads action also is random
                 if ((i % 10000) == 0){
                     Thread.yield();
                 }
